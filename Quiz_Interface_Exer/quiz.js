0e860cd0aaa38385615d4a39f9e4a87c9410f175
ChadWiles <103564240@student.swin.edu.au>
2021-06-07 10:36:58 +1000

    script attached

---
+++ Quiz_Interface_Exer/quiz.js
@@ -0,0 +1,15 @@
+var quiz = {
+    questions: [],
+    unsortedQuestions: [],
+    questionSequence: [],
+};
+
+
+// Question order before its randomized //
+quiz.unsortedQuestions[0]="What is the the name for the Jewish New Year?";
+quiz.unsortedQuestions[1]="How many blue stripes are there on the U.S. flag?";
+quiz.unsortedQuestions[2]="Which one of these characters is not friends with Harry Potter?";
+quiz.unsortedQuestions[3]="What is the color of Donald Duck’s bowtie?";
+quiz.unsortedQuestions[4]="What was the name of the band Lionel Richie was a part of?";
+quiz.unsortedQuestions[5]="Which animal does not appear in the Chinese zodiac?";
+
